718b8da4ebe23b7be2361f746366e3de87384737
Hawkez Haven <hawkezhaven@gmail.com>
2026-08-13 07:43:43 +1200

    Revert incorrect hero banner wiring

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,34 +1,14 @@
-import { useLocation } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar.tsx";
 import Footer from "./Footer.tsx";
 import SEO from "../SEO.tsx";
-import HeroBanner from "./HeroBanner.tsx";
-
-const HERO_PATHS = new Set([
-  "/about",
-  "/horses",
-  "/support",
-  "/contact",
-  "/adoption",
-  "/sponsorship",
-  "/foster",
-  "/volunteer",
-  "/education",
-  "/experiences",
-  "/hub",
-]);
 
 export default function AppLayout() {
-  const { pathname } = useLocation();
-  const hasPageHero = HERO_PATHS.has(pathname);
-
   return (
     <div className="min-h-screen flex flex-col">
       <SEO />
       <Navbar />
-      <main className="flex-1" data-page-hero={hasPageHero ? "true" : undefined}>
-        <HeroBanner />
+      <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
