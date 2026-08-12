e090231731e5a7ae84b28f68645f971a439d407f
Hawkez Haven <hawkezhaven@gmail.com>
2026-08-12 19:17:36 +1200

    Add route-aware SEO to site layout

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar.tsx";
 import Footer from "./Footer.tsx";
+import SEO from "../SEO.tsx";
 
 export default function AppLayout() {
   return (
     <div className="min-h-screen flex flex-col">
+      <SEO />
       <Navbar />
       <main className="flex-1">
         <Outlet />
