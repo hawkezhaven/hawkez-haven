8d604baae5cdca37cf0dd97d4859948d9dea8aed
Hawkez Haven <hawkezhaven@gmail.com>
2026-08-17 01:09:30 +1200

    Add route-aware page classes for hero styling

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { useLocation, Outlet } from "react-router-dom";
 import Navbar from "./Navbar.tsx";
 import Footer from "./Footer.tsx";
 import SEO from "../SEO.tsx";
 
 export default function AppLayout() {
+  const { pathname } = useLocation();
+  const pageClass = `page-${pathname.replace(/^\//, "").replace(/\//g, "-") || "home"}`;
+
   return (
     <div className="min-h-screen flex flex-col">
       <SEO />
       <Navbar />
-      <main className="flex-1">
+      <main className={`flex-1 ${pageClass}`}>
         <Outlet />
       </main>
       <Footer />
