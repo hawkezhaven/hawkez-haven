b8cc26724cccc57915206919d50e593ff83aa9eb
Hawkez Haven <hawkezhaven@gmail.com>
2026-08-31 22:36:19 +1200

    Add Gift Cards to site navigation

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -7,6 +7,7 @@ const NAV_LINKS = [
   { label: "About", href: "/about" },
   { label: "Horses", href: "/horses" },
   { label: "Support Us", href: "/support" },
+  { label: "Gift Cards", href: "/gift-card" },
   { label: "Contact", href: "/contact" },
 ];
 
@@ -15,7 +16,7 @@ const MORE_LINKS = [
   { label: "Sponsorship", href: "/sponsorship" },
   { label: "Foster Program", href: "/foster" },
   { label: "Volunteer", href: "/volunteer" },
-{ label: "Experiences & Lessons", href: "/experiences" }, 
+  { label: "Experiences & Lessons", href: "/experiences" },
   { label: "The Hub", href: "/hub" },
 ];
 
