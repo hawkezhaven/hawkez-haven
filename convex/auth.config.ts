40d44f312f1ee1786f350f29896367a0864f13d3
Hawkez Haven <hawkezhaven@gmail.com>
2026-09-03 21:59:56 +1200

    Remove retired provider reference from auth config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,6 @@
 import { AuthConfig } from "convex/server";
 
 // Authentication is not currently used by Hawkez Haven.
-// Keep the Convex auth config valid without the retired Hercules/OIDC provider.
 export default {
   providers: [],
 } satisfies AuthConfig;
